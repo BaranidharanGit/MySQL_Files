b0c7c7e28d7fcd240f44737753e634d2b58e48dc
Baranidharan Boopathi <baranidharanboopathi@Baranidharans-MacBook-Air.local>
2025-07-01 08:42:11 +0530

    Create coffe store database queries

--- SQL_Basics.sql
+++ SQL_Basics.sql
@@ -1,1 +1,28 @@
 /* SQL Basics */
+
+CREATE DATABASE coffee_store;
+
+USE coffee_store;
+
+CREATE TABLE products(
+	id INT auto_increment PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    price DECIMAL(3,2)
+);
+
+CREATE TABLE customers(
+	id INT auto_increment PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30),
+    gender ENUM('M','F'),
+    phonenumber VARCHAR(15)
+);
+
+CREATE TABLE orders(
+	id INT auto_increment PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    order_time DATETIME,
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
